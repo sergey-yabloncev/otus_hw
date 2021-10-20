8ff770e09febde714e3718f5b01b7ee84a3dae80
Sergey Yabloncev <sergey@4px.ru>
2021-10-20 11:34:19 +0300

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,105 @@ package main
 
 import (
 	"errors"
+	"io"
+	"io/fs"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrInfoFile              = errors.New("unable to get information about the file")
+	ErrNotFoundFile          = errors.New("file reading error")
+	ErrCopy                  = errors.New("copy error")
 )
 
-func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+type Helper struct {
+	from *os.File
+	to   *os.File
+	info fs.FileInfo
+}
+
+func (h *Helper) Open(fromPath string, offset int64) error {
+	f, err := os.Open(fromPath)
+	if err != nil {
+		return ErrNotFoundFile
+	}
+
+	fi, err := f.Stat()
+	if err != nil {
+		return ErrInfoFile
+	}
+
+	if fi.IsDir() || fi.Size() == 0 {
+		return ErrUnsupportedFile
+	}
+
+	if offset >= fi.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	f.Seek(offset, 0)
+
+	h.from = f
+	h.info = fi
+
+	return nil
+}
+
+func (h *Helper) Create(toPath string) error {
+	out, err := os.Create(toPath)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+
+	h.to = out
+
+	return nil
+}
+
+func (h *Helper) Copy(limit int64) error {
+	bar := pb.Full.Start64(limit)
+	barReader := bar.NewProxyReader(h.from)
+
+	if _, err := io.CopyN(h.to, barReader, limit); err != nil {
+		return ErrCopy
+	}
+
+	bar.Finish()
+
+	return nil
+}
+
+func Copy(fromPath string, toPath string, offset, limit int64) error {
+	helper := Helper{}
+	err := helper.Open(fromPath, offset)
+	defer helper.from.Close()
+	defer helper.to.Close()
+
+	if err != nil {
+		return err
+	}
+
+	countToCopy := helper.info.Size() - offset
+
+	if limit > 0 && countToCopy > limit {
+		countToCopy = limit
+	}
+
+	err = helper.Create(toPath)
+
+	if err != nil {
+		return err
+	}
+
+	err = helper.Copy(countToCopy)
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,109 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"testing"
 
-func TestCopy(t *testing.T) {
-	// Place your code here.
+	"github.com/stretchr/testify/require"
+)
+
+type test struct {
+	from   string
+	to     string
+	offset int64
+	limit  int64
+	err    error
+	result string
+}
+
+func compare(originalFilePath string, copyFilePath string) (bool, error) {
+	originalFile, err := ioutil.ReadFile(originalFilePath)
+	if err != nil {
+		return false, err
+	}
+
+	copyFile, err := ioutil.ReadFile(copyFilePath)
+	if err != nil {
+		return false, err
+	}
+
+	return bytes.Equal(originalFile, copyFile), nil
+}
+
+func TestSuccess(t *testing.T) {
+	defer os.Remove("out.txt")
+	for _, tst := range [...]test{
+		{
+			from:   "testdata/input.txt",
+			to:     "out.txt",
+			result: "testdata/out_offset0_limit0.txt",
+		},
+		{
+			from:   "testdata/input.txt",
+			to:     "out.txt",
+			limit:  10,
+			result: "testdata/out_offset0_limit10.txt",
+		},
+		{
+			from:   "testdata/input.txt",
+			to:     "out.txt",
+			limit:  1000,
+			result: "testdata/out_offset0_limit1000.txt",
+		},
+		{
+			from:   "testdata/input.txt",
+			to:     "out.txt",
+			limit:  10000,
+			result: "testdata/out_offset0_limit10000.txt",
+		},
+		{
+			from:   "testdata/input.txt",
+			to:     "out.txt",
+			limit:  1000,
+			offset: 100,
+			result: "testdata/out_offset100_limit1000.txt",
+		},
+		{
+			from:   "testdata/input.txt",
+			to:     "out.txt",
+			limit:  1000,
+			offset: 6000,
+			result: "testdata/out_offset6000_limit1000.txt",
+		},
+	} {
+		t.Run("ok", func(t *testing.T) {
+			err := Copy(tst.from, tst.to, tst.offset, tst.limit)
+			require.Nil(t, err)
+
+			filesEqual, err := compare(tst.to, tst.result)
+			require.Nil(t, err)
+
+			require.True(t, filesEqual)
+		})
+	}
+}
+
+func TestFail(t *testing.T) {
+	for _, tst := range [...]test{
+		{
+			from: "file.txt",
+			err:  ErrNotFoundFile,
+		},
+		{
+			from:   "testdata/input.txt",
+			offset: 1000000,
+			err:    ErrOffsetExceedsFileSize,
+		},
+		{
+			from: "testdata",
+			err:  ErrUnsupportedFile,
+		},
+	} {
+		t.Run("fail", func(t *testing.T) {
+			err := Copy(tst.from, tst.to, tst.offset, tst.limit)
+			require.Equal(t, err, tst.err)
+		})
+	}
 }
